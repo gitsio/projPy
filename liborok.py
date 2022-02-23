aae41c32ac29efc6fef099b471a51f05c4a9de82
gitsio <100289789+gitsio@users.noreply.github.com>
2022-02-23 16:20:18 +0300

    Add Liborok

---
+++ liborok.py
@@ -0,0 +1,15 @@
+from colorama import Fore
+def setcolor(color):
+  if (color == "blue"):
+    return Fore.BLUE
+  elif (color == "red"):
+    return Fore.RED
+  elif (color == "green"):
+    return Fore.GREEN
+  elif (color == ""):
+    return Fore.RESET
+  elif (color == "yellow"):
+    return Fore.YELLOW
+  elif (color == "pink"):
+    return Fore.LIGHTMAGNETTA_EX
+  
